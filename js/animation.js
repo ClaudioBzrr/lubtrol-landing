b4bb8e61bf13acb31100e77ffb3ff875e52cbefc
ClaudioBzrr <claudio.bezerra1998@gmail.com>
2021-04-12 00:34:22 -0300

    animações de scroll incompletas

--- js/animation.js
+++ js/animation.js
@@ -6,7 +6,7 @@ function animeScrol() {
     const windowTop = window.pageYOffset
 
     target.forEach(function (e) {
-        if (windowTop > e.offsetTop * 0.66) {
+        if (windowTop > e.offsetTop *1.25) {
             e.classList.add(animationClass)
         } else {
             e.classList.remove(animationClass)
