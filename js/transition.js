faa5be0edb37103d2d3a53c9a82c5bf192f9ddfc
claudiobzrr <claudio.bezerra1998@gmail.com>
2021-06-17 14:19:56 -0300

    fix: linha copyright corrigida e links adicionados

--- js/transition.js
+++ js/transition.js
@@ -12,6 +12,7 @@ export const Fade = class extends Highway.Transition {
         const tl = new TimelineLite();
         const footer = document.querySelector('.footer-content')
         const mural =  document.querySelector('.mural')
+        const copyright =  document.querySelector('.copyright')
         tl.fromTo(to, 0.5, { left: '-100%', top: '50%' }, { left: '0%' })
             .fromTo(to, 0.5, { height: '1vh' },
                 {
@@ -26,6 +27,7 @@ export const Fade = class extends Highway.Transition {
             .fromTo(products,0.5,{opacity:0},{opacity:1})
             .fromTo(footer, 0.1, { opacity: 0 }, { opacity: 1 })
             .fromTo(wpp, 0.2, { opacity: 0 }, {opacity: 1 })
+            .fromTo(copyright, 0.2, { opacity: 0 }, {opacity: 1 })
 
     }
     out({ from, done }) {
