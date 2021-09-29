f46b222eaa21434e918dee4eb6147cb3160e7293
claudiobzrr <claudio.bezerra1998@gmail.com>
2021-09-29 14:32:47 -0300

    imagen da página "sobre" alterada de posição.

--- js/transition.js
+++ js/transition.js
@@ -5,6 +5,7 @@ import { TimelineLite } from 'gsap';
 
 export const Fade = class extends Highway.Transition {
     in({ from, to, done }) {
+        const about_content = document.querySelector('.about-content') 
         const brandshell =  document.querySelector('.home-brand-shell')
         const brand = document.querySelector('.brand')
         const products =  document.querySelector('.home-products')
@@ -13,6 +14,12 @@ export const Fade = class extends Highway.Transition {
         const footer = document.querySelector('.footer-content')
         const mural =  document.querySelector('.mural')
         const copyright =  document.querySelector('.copyright')
+
+        function verifyChild(el){
+            if(el != about_content){
+                return el
+            }
+        }
         tl.fromTo(to, 0.5, { left: '-100%', top: '50%' }, { left: '0%' })
             .fromTo(to, 0.5, { height: '1vh' },
                 {
@@ -20,7 +27,11 @@ export const Fade = class extends Highway.Transition {
                         done();
                     }
                 })
-            .fromTo(to.children[0], 0.4, { bottom: '-100%', opacity: 0 }, { bottom: '0%', opacity: 1 })
+
+// (to.children[0]!=about_content)
+                .fromTo(verifyChild(to.children[0]), 0.4, { bottom: '-100%', opacity: 0 }, { bottom: '0%', opacity: 1 })
+                .fromTo(about_content, 0.4, { bottom: '-100%'}, { bottom: '0%'})
+
             .fromTo(brand, 0.5, { bottom: '-100%', opacity: 0 }, { top: '0.06vh', opacity: 1 })
             .fromTo(mural, 0.5, { bottom: '-100%', opacity: 0 }, { top: '0.06vh', opacity: 1 })
             .fromTo(brandshell, 0.5, {opacity: 0 }, {opacity: 1 })
